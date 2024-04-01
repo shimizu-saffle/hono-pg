47943ebbf24f25a06efb8f905363dc9b2e0893c0
shimizu-saffle <shimizu.saffle@gmail.com>
2024-04-01 22:38:59 +0900

    chore: add dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { z } from 'zod';
 
 const app = new Hono();
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: `Hello!`,
-  });
-});
+const route = app.get(
+  '/hello',
+  zValidator(
+    'query',
+    z.object({
+      name: z.string(),
+    })
+  ),
+  (c) => {
+    const { name } = c.req.valid('query');
+    return c.json({
+      message: `Hello! ${name}`,
+    });
+  }
+);
 
-export default app;
+export type AppType = typeof route;
